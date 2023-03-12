9662cb78a422af2be24fa9f4f4c288abcda3ccf6
123 <chap7@mail.ru>
2023-03-12 16:15:04 +0300

    HW Память Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import javax.swing.*;
+import java.sql.Array;
+import java.util.Arrays;
+
 public class Main {
+    //Задание 1
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person = new Person("Lyapis", "Trubetskoy");
+        changePerson(person);
+        System.out.println(person);
     }
+
+
+    private static void changePerson(Person person) {
+//        person = new Person();
+        person.name = "Ilya";
+        person.surname = "Lagutenko";
+
+    }
+
+
+
 }
